2ffc8c71ba163209a55302a51a2219c49986bbfe
Hodorth <xthxmx0120@gmail.com>
2025-04-10 12:00:50 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target:  'https://scai-ai-league-production-2128.up.railway.app' || 'http://localhost:4000', 
+        target:  'https://scai-ai-league-production-2128.up.railway.app', 
         changeOrigin: true,
       },
     },
