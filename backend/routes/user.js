83b845400f06da4dbbcd0d6a2f5cc62ee081d54c
Hodorth <xthxmx0120@gmail.com>
2025-04-10 11:29:21 +0300

    test

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,9 +2,6 @@ const express = require("express")
 const router = express.Router()
 const { loginUser, signupUser } = require('../controllers/userController')
 
-router.get("/",(req,res)=>{
-    res.json({mssg:"get users"})
-})
 router.post("/login",loginUser)
 router.post("/signup",signupUser)
 
